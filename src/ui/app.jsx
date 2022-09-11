a4814297f48a51ab8c6991b5da504aa28110831a
master <hulktv222@mail.ru>
2022-09-11 19:32:12 +0300

    init commit

---
+++ src/ui/app.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function render() {
+  ReactDOM.render(<h2>Hello from React to Natasha!</h2>, document.body);
+}
+
+render();
